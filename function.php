3c43717527decb8f8481cb2ee4123aabef3d1328
kabayan349 <67649284+kabayan349@users.noreply.github.com>
2020-07-22 00:31:17 +0700

    kfjd

--- function.php
+++ function.php
@@ -5,14 +5,13 @@ function request($url, $token = null, $data = null, $pin = null, $otpsetpin = nu
 $header[] = "Host: api.gojekapi.com";
 $header[] = "User-Agent: okhttp/3.10.0";
 $header[] = "Accept: application/json";
-$header[] = "Accept-Language: id-ID";
+$header[] = "Accept-Language: en-EN";
 $header[] = "Content-Type: application/json; charset=UTF-8";
-$header[] = "X-AppVersion: 3.30.2";
+$header[] = "X-AppVersion: 3.51.4";
 $header[] = "X-UniqueId: ".time()."57".mt_rand(1000,9999);
 $header[] = "Connection: keep-alive";
 $header[] = "X-User-Locale: id_ID";
-$header[] = "X-Location: -8.340540,115.091952";
-$header[] = "X-Location-Accuracy: 3.0";
+$header[] = "X-Location: -7.38".mt_rand(1000,9999).",112.73".mt_rand(1000,9999);
 if ($pin):
 $header[] = "pin: $pin";
     endif;
