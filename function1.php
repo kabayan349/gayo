0b198558b73d7865ec49d19fbfab01b98387cdef
kabayan349 <67649284+kabayan349@users.noreply.github.com>
2020-07-23 01:06:18 +0700

    ugyf

--- function1.php
+++ function1.php
@@ -11,7 +11,7 @@ function request($url, $token = null, $data = null, $pin = null, $otpsetpin = nu
     $header[] = "X-UniqueId: ".time()."57".mt_rand(1000,9999);
     $header[] = "Connection: keep-alive";
     $header[] = "X-User-Locale: id_ID";
-    $header[] = "X-Location: -7.320477,108.8330";
+    $header[] = "X-Location: -8.589423,116.122975";
     $header[] = "X-Location-Accuracy: 3.0";
     if ($pin):
     $header[] = "pin: $pin";
